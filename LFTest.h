356da48a3351fbea59251d9dd6af7dc50a0a8f29
Luis Figueroa <cooper@Luiss-MacBook-Pro.local>
2014-05-22 23:19:03 -0400

    I just added a test class

---
+++ LFTest.h
@@ -0,0 +1,13 @@
+//
+//  LFTest.h
+//  Git Test
+//
+//  Created by Luis Figueroa on 5/22/14.
+//  Copyright (c) 2014 Luis Figueroa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LFTest : NSObject
+
+@end
---
+++ LFTest.m
@@ -0,0 +1,13 @@
+//
+//  LFTest.m
+//  Git Test
+//
+//  Created by Luis Figueroa on 5/22/14.
+//  Copyright (c) 2014 Luis Figueroa. All rights reserved.
+//
+
+#import "LFTest.h"
+
+@implementation LFTest
+
+@end
